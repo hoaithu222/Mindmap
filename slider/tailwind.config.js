2db94ef849d86812582f3d0ce43dfdd977e2ac56
Thu222 <thu601925@gmail.com>
2024-12-07 17:20:53 +0700

    add

--- slider/tailwind.config.js
+++ slider/tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
+    './src/**/*.{js,jsx,ts,tsx}', // Đảm bảo đường dẫn đúng với dự án
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)", 
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
